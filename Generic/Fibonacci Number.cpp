1d59f4f750b8110061b6aadcc9ee4fe9035cc306
Vaibhav Sachdeva <vsachdeva4859@gmail.com>
2025-02-12 12:20:44 +0530

    Create Fibonacci Number.cpp

---
+++ Generic/Fibonacci Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0) return 0;
+        int a = 0, b = 1;
+        for(int i = 2; i <= n; i++) {
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+
+        return b;
+    }
+};
+
+// https://leetcode.com/problems/fibonacci-number/

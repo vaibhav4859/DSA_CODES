354ade0c070895fb2c94e00b48c27820553bcc0f
Vaibhav Sachdeva <vsachdeva4859@gmail.com>
2025-02-15 22:11:57 +0530

    Update Kadane's Algo Maximum Sum Subarray.cpp

--- Arrays/Kadane's Algo Maximum Sum Subarray.cpp
+++ Arrays/Kadane's Algo Maximum Sum Subarray.cpp
@@ -1,4 +1,5 @@
 // Given an integer array nums, find the subarray with the largest sum, and return its sum.
+// ** IMP
 
 class Solution {
     public int maxSubArray(int[] nums) {
@@ -10,4 +11,29 @@ class Solution {
         }
         return ans;
     }
+}
+
+
+// print maximum sum subarray also
+pair<int, vector<int>> maxSubarraySum(vector<int>& arr) {
+    int maxSum = arr[0], currentSum = arr[0];
+    int start = 0, end = 0, tempStart = 0;
+    
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] > currentSum + arr[i]) {
+            currentSum = arr[i];
+            tempStart = i;
+        } else {
+            currentSum += arr[i];
+        }
+
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+            start = tempStart;
+            end = i;
+        }
+    }
+
+    vector<int> subarray(arr.begin() + start, arr.begin() + end + 1);
+    return {maxSum, subarray};
 }
